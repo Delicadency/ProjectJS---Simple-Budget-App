71a14efc6d9eaf38cdba72644081d6747940b51c
Agata Kamińska <a.fluffypie@gmail.com>
2024-07-11 00:10:24 +0200

    edit and delete buttons created, const names changed, inputs values reset

--- JS/script.js
+++ JS/script.js
@@ -1,21 +1,21 @@
 "use strict";
 let counter = 0;
-const buttonTypes = ["income", "outcome"];
-buttonTypes.forEach((buttonType) => {
-  document.getElementById(`intput-button-${buttonType}`).addEventListener(
-    ("click",
-    (event) => {
+const types = ["income", "outcome"];
+types.forEach((type) => {
+  document
+    .getElementById(`input-button-${type}`)
+    .addEventListener("click", (event) => {
       event.preventDefault();
       counter++;
-      const getIcomeName = document.getElementById("income-name").value;
-      const getIncomeAmount = doument.getElementbyIt("income-amount").value;
+      const inputName = document.getElementById(`${type}-name`).value;
+      const inputAmount = document.getElementById(`${type}-amount`).value;
       const li = document.createElement("li");
       li.className = "flex a-i--center list__element";
       li.id = `list-element-${counter}`;
       const listParagraph = document.createElement("p");
       listParagraph.className = "list__paragraph";
       listParagraph.id = `list-parahraph-${counter}`;
-      listParagraph.textContent = `${getIncomeAmount} - ${getIcomeName}`;
+      listParagraph.textContent = `${inputAmount} zł - ${inputName}`;
       const listDiv = document.createElement("div");
       listDiv.className =
         "flex j-c--between a-i--center list__button_container";
@@ -23,7 +23,20 @@ buttonTypes.forEach((buttonType) => {
       const editButton = document.createElement("button");
       editButton.type = "submit";
       editButton.name = "edit";
-      editButton.className = "list__button edit edit--income";
-    })
-  );
+      editButton.className = `list__button edit edit--${type}`;
+      editButton.id = `button-edit-${counter}`;
+      const deleteButton = document.createElement("button");
+      deleteButton.type = "submit";
+      deleteButton.name = "delete";
+      deleteButton.className = `list__button delete delete--${type}`;
+      deleteButton.id = `button-delete-${type}`;
+      const ul = document.getElementById(`${type}-list`);
+      ul.appendChild(li);
+      li.appendChild(listParagraph);
+      li.appendChild(listDiv);
+      listDiv.appendChild(editButton);
+      listDiv.appendChild(deleteButton);
+      document.getElementById(`${type}-name`).value = "";
+      document.getElementById(`${type}-amount`).value = "";
+    });
 });
