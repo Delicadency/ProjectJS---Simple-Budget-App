e454c55e61d32b58d6c5294f731efd8d3d5371cb
Agata Kamińska <a.fluffypie@gmail.com>
2024-07-11 13:33:00 +0200

    input length check

--- JS/script.js
+++ JS/script.js
@@ -3,12 +3,41 @@ let counter = 0;
 const types = ["income", "outcome"];
 types.forEach((type) => {
   document
+    .getElementById(`${type}-amount`)
+    .addEventListener("input", (event) => {
+      event.preventDefault();
+      const input = event.target;
+      let value = input.value;
+      const regex = /^\d{0,7}(\.\d{0,2})?$/;
+      if (!regex.test(value)) {
+        value = value.slice(0, -1);
+        input.value = value;
+      }
+    });
+
+  document.getElementById(`${type}-name`).addEventListener("input", (event) => {
+    event.preventDefault();
+    const input = event.target;
+    let value = input.value;
+    const regex = /^.{0,30}$/;
+    if (!regex.test(value)) {
+      (value = value.slice(0, 30)), (input.value = value);
+    }
+  });
+
+  document
     .getElementById(`input-button-${type}`)
     .addEventListener("click", (event) => {
       event.preventDefault();
       counter++;
       const inputName = document.getElementById(`${type}-name`).value;
       const inputAmount = document.getElementById(`${type}-amount`).value;
+      if (inputName === "" || inputAmount === "") {
+        console.error(
+          "Tu wstaw element z komunikatem o konieczności uzupełnienia obu pól."
+        );
+        return;
+      }
       const li = document.createElement("li");
       li.className = "flex a-i--center list__element";
       li.id = `list-element-${counter}`;
